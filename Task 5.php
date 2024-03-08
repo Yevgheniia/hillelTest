197b277b2bf4cd847b00fcadb80e20ce9536256b
Yevheniia Mitrokhina <etupik13@gmail.com>
2024-03-08 12:42:32 +0100

    Task 5.1

--- Task 5.php
+++ Task 5.php
@@ -1,1 +1,46 @@
 <?php
+function FindElem($arr, $num)
+{
+    if(is_array($arr)){
+
+      $result = [];
+    if(isset($arr,$num)){
+
+        $result[0] = $arr[$num];
+    }
+        foreach ($arr as  $value ){
+            if (is_array($value)){
+                $result = array_merge($result,FindElem($value,$num));
+            }
+        }
+    }
+
+
+    return $result;
+}
+$arr = [ 1,5,11=>[111,3,4], 2, 33=>[55,66,77], 7, 31, 4, 1111, 8=>[11111,222,333,444], 6];
+
+print_r(FindElem($arr,1));
+
+function CountLet($line)
+{
+    if (is_string($line)){
+        $count =0;
+        $array = str_split($line);
+
+        foreach ($array as $value){
+            if ($value=='b'){
+                $count++;
+            }
+        }
+
+    }
+    else{
+        return false;
+    }
+    return $count;
+
+}
+
+echo '_________________'.CountLet('wkibchbjwikbid');
+
